0d723e8ff4f772826208949d5d55b357f28ce622
Amagin0 <amaginyos.ask.given.u@gmail.com>
2022-04-21 09:17:40 +0000

    [save]

--- main.php
+++ main.php
@@ -4,32 +4,56 @@
 require_once('./classes/Human.php');
 require_once('./classes/Enemy.php');
 require_once('./classes/Brave.php');
+require_once('./classes/BlackMage.php');
+require_once('./classes/WhiteMage.php');
 
 /* インスタンス化 */
-$player = new Brave("勇者");
-$goblin = new Enemy("ゴブリン");
+$members = array();
+$members[] = new Brave("勇者");
+$members[] = new BlackMage("黒魔術師");
+$members[] = new WhiteMage("白魔導士");
 
-// $player->name = "勇者";
-// $goblin->name = "ゴブリン";
+$enemies = array();
+$enemies[] = new Enemy("ゴブリン", 20);
+$enemies[] = new Enemy("ボム", 25);
+$enemies[] = new Enemy("モルボル", 30);
+
+// $member->name = "勇者";
+// $enemy->name = "ゴブリン";
 
 $turn = 1;
 
 /* どちらかのHPが0になるまで繰り返す */
-while ($player->getHitPoint() > 0 && $goblin->getHitPoint() > 0) {
+while ($member->getHitPoint() > 0 && $enemy->getHitPoint() > 0) {
   /* ターン数の表示 */
   echo "*** $turn ターン目 *** \n\n";
   
   /* 現在のHPの表示 */
-  echo $player->getName() . "  :  " . $player->getHitPoint() . "/" . $player::MAX_HITPOINT . "\n";  
-  echo $goblin->getName() . "  :  " . $goblin->getHitPoint() . "/" . $goblin::MAX_HITPOINT . "\n";
-  echo "\n";
+  foreach($members as $member) {
+     echo $member->getName() . "  :  " . $member->getHitPoint() . "/" . $member::MAX_HITPOINT . "\n";  
+     echo "\n";
+  }
+  
+  foreach($enemies as $enemy) {
+     echo $enemy->getName() . "  :  " . $enemy->getHitPoint() . "/" . $enemy::MAX_HITPOINT . "\n";
+     echo "\n";
+  }
   // constで定義したものはオブジェクト定数
-  // オブジェクト定数は "::" で参照するため "$player::MAX_HITPOINT" と呼び出す
+  // オブジェクト定数は "::" で参照するため "$member::MAX_HITPOINT" と呼び出す
   
   /* 攻撃 */
-  $player->doAttack($goblin);
-  echo "\n";
-  $goblin->doAttack($player);
+  foreach($members as $member) {
+    $enemyIndex = rand(0, count($enemies) - 1);
+    $enemy = $enemies[$enemyIndex];
+    
+    if (get_class($member) == "WhiteMage") {
+      $member->doAttackWhiteMage($enemy, $member);
+    } else {
+      $member->doAttack($enemy);
+    }
+  }
+  
+  $enemy->doAttack($member);
   echo "\n";
   
   $turn++;
@@ -37,5 +61,5 @@ while ($player->getHitPoint() > 0 && $goblin->getHitPoint() > 0) {
 }
 
 echo "<<<戦闘終了！>>>\n\n";
-echo $player->getName() . "  :  " . $player->getHitPoint() . "/" . $player::MAX_HITPOINT . "\n";  
-echo $goblin->getName() . "  :  " . $goblin->getHitPoint() . "/" . $goblin::MAX_HITPOINT . "\n\n";
+echo $member->getName() . "  :  " . $member->getHitPoint() . "/" . $member::MAX_HITPOINT . "\n";  
+echo $enemy->getName() . "  :  " . $enemy->getHitPoint() . "/" . $enemy::MAX_HITPOINT . "\n\n";
